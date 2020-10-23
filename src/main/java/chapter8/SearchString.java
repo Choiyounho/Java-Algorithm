eb59c9400127f2d4fb553446e1691e9c5505e042
Choiyounho <maxosa78@gmail.com>
2020-10-23 18:21:47 +0900

    [feat] : 문자열 검색 Boyer-Moore법 구현

--- src/main/java/chapter8/SearchString.java
+++ src/main/java/chapter8/SearchString.java
@@ -4,7 +4,7 @@ public class SearchString {
 
     public static void main(String[] args) {
 
-        // 문자열 검색 브루트-포스트 법
+        // 문자열 검색 브루트-포스트법
         String s = "AB자바CDE자바FG자바HIJ자바";
 //        System.out.println(s.indexOf("자바"));
 //        System.out.println(s.lastIndexOf("자바"));
@@ -14,17 +14,26 @@ public class SearchString {
         String s1 = "ZABCABXACCADEFABCABD";
         String s2 = "ABCABD";
         int index = kmpMatch(s1, s2);
-        if (index == -1)
+        printMatch(s1, s2, index);
+
+        // Boyer-Moore법
+        index = 0;
+        index = bmMatch(s1, s2);
+        printMatch(s1, s2, index);
+
+    }
+
+    private static void printMatch(String s1, String s2, int index) {
+        if (index == -1) {
             System.out.println("텍스트에 패턴이 없습니다.");
-        if (index != -1){
+        }
+        if (index != -1) {
             int len = 0;
             for (int i = 0; i < index; i++)
                 len += s1.substring(i, i + 1).getBytes().length;
             len += s2.length();
             System.out.println((index + 1) + "번째 문자와 일치합니다.");
         }
-
-
     }
 
     private static int kmpMatch(String txt, String pattern) {
@@ -55,6 +64,32 @@ public class SearchString {
 
         if (pointPattern == pattern.length())
             return pointTxt - pointPattern;
+        return -1;
+    }
+
+    private static int bmMatch(String txt, String pat) {
+        int pt;
+        int pp;
+        int txtLen = txt.length();
+        int patLen = pat.length();
+        int[] skip = new int[Character.MAX_VALUE + 1];
+
+        for (pt = 0; pt <= Character.MAX_VALUE; pt++)
+            skip[pt] = patLen;
+        for (pt = 0; pt < patLen - 1; pt++)
+            skip[pat.charAt(pt)] = patLen - pt - 1;
+
+        while (pt < txtLen) {
+            pp = patLen - 1;
+
+            while (txt.charAt(pt) == pat.charAt(pp)) {
+                if (pp == 0)
+                    return pt;
+                pp--;
+                pt--;
+            }
+            pt += (skip[txt.charAt(pt)] > patLen - pp) ? skip[txt.charAt(pt)] : patLen - pp;
+        }
         return -1;
     }
 
